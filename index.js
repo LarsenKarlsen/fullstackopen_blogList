617ea65e55af623fcc7a92c8da0a41c0789b55f4
A.N. Protasevich <larsplotner@gmail.com>
2023-04-10 20:01:12 +0300

    refactor: linting

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const app = require("./app")
+const config = require("./utils/config")
+const logger = require("./utils/logger")
 
-app.listen(config.PORT, () => {
+const PORT = config.PORT
+app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,3 @@
-const { request, response } = require("express")
 const logger = require("./logger")
 
 const requestLogger = (request, response, next) => {
@@ -7,15 +6,15 @@ const requestLogger = (request, response, next) => {
 }
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: "unknown endpoint" })
 }
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message })
   }
 
