5edc57161dab1570eadce66c391575ea14112c27
A.N. Protasevich <larsplotner@gmail.com>
2023-04-10 19:30:38 +0300

    refactor: app structure and models

--- models/blog.js
+++ models/blog.js
@@ -1,24 +1,19 @@
 const mongoose = require('mongoose')
 
-const config = require('../utils/config')
-const logger = require('../utils/logger')
-
-const mongoUrl = config.MONGODB_URI
-
-logger.info("Connecting to ", mongoUrl)
-mongoose.connect(mongoUrl)
-.then(()=>{
-  logger.info("Connected to MongoDB")
-})
-.catch(error => {
-  logger.error("Error connecting to MongoDB\n", error)
-})
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
+})
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject)=>{
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
