775ca4d74f9e6b8272a85bfa762c2c20436f7ebc
A.Protasevich <larsplotner@gmail.com>
2023-04-29 17:35:28 +0300

    feat: 5.7-5.8

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [detailsVisibility, setDetailsVisibility] = useState(false)
+
+  const toggleBlogDetails = () => {
+    setDetailsVisibility(!detailsVisibility)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showDetails = () => {
+    return (
+      <div>
+        <div><a href={blog.url}>{blog.url}</a></div>
+        <div>
+          Likes:{blog.likes} <span><button>Like</button></span>
+        </div>
+      </div>
+    )
+  }
+  
+  return (
+  <div style={blogStyle}>
+    <div>{blog.title} by {blog.author}</div>
+    <button onClick={toggleBlogDetails}>{detailsVisibility ? "hide" : "show"}</button>
+    {detailsVisibility && showDetails()}
+  </div>
+)}
 
 export default Blog
