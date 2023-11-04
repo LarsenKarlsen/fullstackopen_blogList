a867122656f88e1b548240fba321b475b3a44ca0
A.Protasevich <larsplotner@gmail.com>
2023-11-04 17:34:24 +0300

    fix: useFild hook initial value

--- bloglist-frontend/src/hooks/index.js
+++ bloglist-frontend/src/hooks/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export const useField = (type) => {
-  const [value, setValue] = useState()
+  const [value, setValue] = useState("")
 
   const onChange = (event) => {
     setValue(event.target.value)
