22167bcd4131aa9ac7cd1fc61093f1e3584b7e95
A.Protasevich <larsplotner@gmail.com>
2023-11-04 18:07:25 +0300

    refactor: blogFrom component

--- bloglist-frontend/src/components/BlogForm.js
+++ bloglist-frontend/src/components/BlogForm.js
@@ -1,72 +1,40 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux"
+import { useField } from "../hooks"
+import { Form, Button } from "react-bootstrap"
 
 import { addBlog } from "../reducers/blogReducer"
 
 const BlogForm = () => {
+  const title = useField("text")
+  const author = useField("text")
+  const url = useField("text")
   const [newBlog, setNewBlog] = useState({ title: "", url: "", author: "" })
   const dispatch = useDispatch()
 
   const handleNewBlogSubmit = (event) => {
     event.preventDefault()
+    setNewBlog({ title: title.value, url: url.value, author: author.value })
     dispatch(addBlog(newBlog))
     setNewBlog({ title: "", url: "", author: "" })
   }
 
   return (
-    <div>
-      <form onSubmit={handleNewBlogSubmit}>
-        <h3>Add new blog</h3>
-        <div>
-          Title:{" "}
-          <input
-            type="text"
-            value={newBlog.title}
-            id="blogform-title"
-            onChange={({ target }) =>
-              setNewBlog({
-                title: target.value,
-                url: newBlog.url,
-                author: newBlog.author,
-              })
-            }
-          />
-        </div>
-        <div>
-          Author:{" "}
-          <input
-            type="text"
-            value={newBlog.author}
-            id="blogform-author"
-            onChange={({ target }) =>
-              setNewBlog({
-                title: newBlog.title,
-                url: newBlog.url,
-                author: target.value,
-              })
-            }
-          />
-        </div>
-        <div>
-          URL:{" "}
-          <input
-            type="text"
-            value={newBlog.url}
-            id="blogform-url"
-            onChange={({ target }) =>
-              setNewBlog({
-                title: newBlog.title,
-                url: target.value,
-                author: newBlog.author,
-              })
-            }
-          />
-        </div>
-        <div>
-          <input type="submit" value="Add" id="blogform-submit" />
-        </div>
-      </form>
-    </div>
+    <Form onSubmit={handleNewBlogSubmit}>
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control type={title.type} onChange={title.onChange} placeholder="Blog title"/>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Author</Form.Label>
+        <Form.Control type={author.type} onChange={author.onChange} placeholder="Blog author"/>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>URL</Form.Label>
+        <Form.Control type={url.type} onChange={url.onChange} placeholder="Blog URL address"/>
+      </Form.Group>
+      <Button variant="outline-primary" type="submit">Submit</Button>
+    </Form>
   )
 }
 
