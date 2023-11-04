8d6167b6c0702b1927d1efe03a6090dede2a2593
A.Protasevich <larsplotner@gmail.com>
2023-11-04 16:44:34 +0300

    feat: logout btn

--- bloglist-frontend/src/components/LogoutBtn.js
+++ bloglist-frontend/src/components/LogoutBtn.js
@@ -1,8 +1,23 @@
 import PropTypes from "prop-types"
+import { Button } from "react-bootstrap"
+import { BoxArrowRight } from "react-bootstrap-icons"
+
+const style = {
+  width: "50px",
+  height: "50px",
+  padding: "7px 10px",
+  borderRadius: "35px",
+  fontSize: "20px",
+  textAlign: "center",
+}
 
 const LogoutBtn = ({ onClick }) => (
   <>
-    <button onClick={onClick}>Logout</button>
+    <Button variant="outline-secondary" onClick={onClick} style={style}>
+      <div className='d-flex justify-content-center'>
+        <BoxArrowRight />
+      </div>
+    </Button>
   </>
 )
 
